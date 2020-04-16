373f31d31b05f0c2e8d0f536dfd3955e54c724cd
luisjavierlora <53449519+luisjavierlora@users.noreply.github.com>
2020-04-16 01:00:35 -0500

    Update Task.kt

--- app/src/main/java/com/example/pruss/model/LocalTask/Task.kt
+++ app/src/main/java/com/example/pruss/model/LocalTask/Task.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "tabla_task")
 class Task (
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
-    @ColumnInfo(name ="titulo") val titulo: String = "",
+    @ColumnInfo(name ="titulo") val titulo: String,
     @ColumnInfo(name ="fecha_entrega") val fecha_en:String,
     @ColumnInfo(name ="hora_entrega") val hora_en:String,
     @ColumnInfo(name ="estado") val est:Int
